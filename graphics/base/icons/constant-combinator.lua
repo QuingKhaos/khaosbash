d5e7e3e8195526b7396358efa79abfbfaf3476ed
Em Karisch <emii.khaos@gmail.com>
2026-07-24 07:41:30 +0200

    refactor: add icon size to image layer definition

--- graphics/base/icons/constant-combinator.lua
+++ graphics/base/icons/constant-combinator.lua
@@ -1,8 +1,8 @@
 --- @type khaosbash.ImageSource
 local image_source = {
   icons = {
-    {filename = "__khaosbash__/graphics/base/icons/constant-combinator-base.png", draw_background = true},
-    {filename = "__khaosbash__/graphics/base/icons/constant-combinator-mask-main-color.png", tint_idx = 1},
+    {filename = "__khaosbash__/graphics/base/icons/constant-combinator-base.png", size = 64, draw_background = true},
+    {filename = "__khaosbash__/graphics/base/icons/constant-combinator-mask-main-color.png", size = 64, tint_idx = 1},
   }
 }
 
