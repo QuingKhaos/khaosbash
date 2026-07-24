be31df76bd349053843ae9a2e4f4175c9787f54f
Em Karisch <emii.khaos@gmail.com>
2026-07-24 20:39:41 +0200

    feat: add ammo technology

---
+++ graphics/base/technology/ammo.lua
@@ -0,0 +1,11 @@
+--- @type khaosbash.ImageSource
+local image_source = {
+  icons = {
+    {filename = "__khaosbash__/graphics/base/technology/ammo-base.png", size = 256},
+    {filename = "__khaosbash__/graphics/base/technology/ammo-mask-magazine-1-type-color.png", size = 256, tint_idx = 1},
+    {filename = "__khaosbash__/graphics/base/technology/ammo-mask-magazine-2-type-color.png", size = 256, tint_idx = 2},
+    {filename = "__khaosbash__/graphics/base/technology/ammo-mask-bullets-type-color.png", size = 256, tint_idx = 3},
+  }
+}
+
+return image_source
