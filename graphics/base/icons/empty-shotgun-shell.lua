c5f2c28d83fe6401d6adbf515d5a1bf4b9c7e415
Em Karisch <emii.khaos@gmail.com>
2026-07-24 14:14:57 +0200

    feat: add shotgun shells

---
+++ graphics/base/icons/empty-shotgun-shell.lua
@@ -0,0 +1,9 @@
+--- @type khaosbash.ImageSource
+local image_source = {
+  icons = {
+    {filename = "__khaosbash__/graphics/base/icons/empty-shotgun-shell-base.png", size = 64, draw_background = true},
+    {filename = "__khaosbash__/graphics/base/icons/empty-shotgun-shell-mask-type-color.png", size = 64, tint_idx = 1},
+  }
+}
+
+return image_source
